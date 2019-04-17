5e3052626619f576dbd9b984dbb4675069aba995
Thomas Senac <senac.seconde@gmail.com>
2019-04-17 18:18:23 +0200

    setup classes + character controller movement

---
+++ Zelda-Like/Assets/Scripts/Classes/LivingClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingClass : MonoBehaviour
+{
+    public float health;
+
+    public virtual void TakeDamages(float damages)
+    {
+        health = health - damages;
+        Debug.Log("Damages Taken");
+    }
+
+    public virtual void Death()
+    {
+        Debug.Log("Dead");
+        Destroy(this.gameObject);
+    }
+}
---
+++ Zelda-Like/Assets/Scripts/Classes/PlayerClass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClass : LivingClass
+{
+    public void PlayerMove(Rigidbody2D rb,float moveX, float moveY,float speed, AnimationCurve accelerationCurve)
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(moveX*speed* accelerationCurve.Evaluate(Time.time), moveY * speed * accelerationCurve.Evaluate(Time.time));
+    }
+
+
+    public void PlayerFirstAttack()
+    {
+
+    }
+
+    public void PlayerSecondAttack()
+    {
+
+    }
+
+    public void PlayerThirdAttack()
+    {
+
+    }
+}
