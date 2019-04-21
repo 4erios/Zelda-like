ce32dbcc636ee49ac89cd49a694f4a788e67010b
Thomas Senac <senac.seconde@gmail.com>
2019-04-22 01:47:19 +0200

    Added: AccelerationMovement, Basic Movements, Player Attack

--- Zelda-Like/Assets/Scripts/Classes/PlayerClass.cs
+++ Zelda-Like/Assets/Scripts/Classes/PlayerClass.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class PlayerClass : LivingClass
 {
-    public void PlayerMove(Rigidbody2D rb,float moveX, float moveY,float speed, AnimationCurve accelerationCurve)
+    public void PlayerAccelerate(Rigidbody2D rb,float moveX, float moveY,float speed, AnimationCurve accelerationCurve)
     {
         moveX = Input.GetAxis("Horizontal");
         moveY = Input.GetAxis("Vertical");
         rb.velocity = new Vector2(moveX*speed* accelerationCurve.Evaluate(Time.time), moveY * speed * accelerationCurve.Evaluate(Time.time));
     }
 
-
-    public void PlayerFirstAttack()
+    public void PlayerMove(Rigidbody2D rb, float moveX, float moveY, float speed)
     {
-
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(moveX * speed, moveY * speed);
     }
 
-    public void PlayerSecondAttack()
+    public void PlayerAttack(Transform AttackPoint, float attackRange,LayerMask enemiesLayer, float playerDamages)
     {
-
+        Collider2D[] enemiesHurt = Physics2D.OverlapCircleAll(AttackPoint.position, attackRange);
+        for (int i=0; i < enemiesHurt.Length; i++)
+        {
+            enemiesHurt[i].GetComponent<LivingClass>().TakeDamages(playerDamages);
+            Debug.Log("Ennemi touché");
+            Debug.Log(i);
+        }
     }
 
-    public void PlayerThirdAttack()
-    {
-
-    }
 }
