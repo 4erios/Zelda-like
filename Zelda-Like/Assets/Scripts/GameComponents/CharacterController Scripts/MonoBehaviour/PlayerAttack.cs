ad7cc38cfca272dc86f5e58ed5a1e47753d0e05a
Aeri0s <senac.seconde@gmail.com>
2019-05-07 22:15:27 +0200

    Retrieve PlayerClass + Reworking PlayerAttack

---
+++ Zelda-Like/Assets/Scripts/GameComponents/CharacterController Scripts/MonoBehaviour/PlayerAttack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [Header("Attack Parameters")]
+    public FloatReference PlayerAttackDamages;
+    public FloatReference AttackRange;
+
+
+    [Header("Energy Parameters")]
+    public FloatVariable EnergyTank;
+
+    public IntVariable CurrentEnergy;
+    public IntReferences EnergyMax;
+
+    
+
+}
