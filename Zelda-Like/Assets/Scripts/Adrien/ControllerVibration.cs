8fe0475fe3c309f3b7d2b0d40ed68d23b45e14a6
Adrien <a.dourgnon@rubika-edu.com>
2019-05-22 17:28:42 +0200

    Tentative de bloquer la caméra

---
+++ Zelda-Like/Assets/Scripts/Adrien/ControllerVibration.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class ControllerVibration : MonoBehaviour {
+
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GamePad.SetVibration(playerIndex, state.Triggers.Left, state.Triggers.Right);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+    }
+}
