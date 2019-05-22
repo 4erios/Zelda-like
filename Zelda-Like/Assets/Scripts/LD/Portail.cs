190e87370b309ba9b26033e1700bc22d9d8cd8fe
Nagashka <e.potdevin@rubika-edu.com>
2019-05-22 02:20:28 +0200

    Scripte Portail + Base script stèle

---
+++ Zelda-Like/Assets/Scripts/LD/Portail.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portail : MonoBehaviour
+{
+    public bool Attack = false;
+
+    private int countDamage;
+    private int portailLife = 5;
+    private int ennemieSelected;
+    private int areaSelected;
+    public float timeBeforeRespawn = 15f;
+    public GameObject[] ennemis; // Préfabs
+    public GameObject[] spawnAreas;
+    private GameObject ennemiObj;
+
+    private void Start()
+    {
+        StartCoroutine(Respawn());
+    }
+
+    void Update()
+    {
+        if (countDamage == portailLife)
+        {
+            // Anim Destroy
+        }
+
+        if (Attack)
+        {
+            countDamage++;
+            Attack = false;
+        }
+    }
+
+    private void Roulette()
+    {
+        ennemieSelected = Random.Range(0, ennemis.Length);
+        areaSelected = Random.Range(0, spawnAreas.Length);
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(timeBeforeRespawn);
+        Roulette();
+        ennemiObj = Instantiate(ennemis[ennemieSelected], spawnAreas[areaSelected].transform.position, Quaternion.identity) as GameObject;
+        StartCoroutine(Respawn());
+    }
+}
---
+++ Zelda-Like/Assets/Scripts/LD/Stèle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stèle : MonoBehaviour
+{
+    public bool Attack = false;
+
+    private GameObject player;
+    [SerializeField]
+    private int scoreWin;
+    public bool Energy;
+
+    public IntVariable current;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (Attack)
+        {
+            if (Energy)
+            {
+                //current += scoreWin;
+                //Start annim
+            }
+
+            else
+            {
+                //current += scoreWin;
+                //Start Annim
+            }
+        }
+
+    }
+}
