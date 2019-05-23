50866d35831ab075e025175cb8cc8b5f88602458
Nagashka <e.potdevin@rubika-edu.com>
2019-05-23 21:24:22 +0200

    UI Mb + Boss

---
+++ Zelda-Like/Assets/Scripts/UI/Enemis_UI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemis_UI : MonoBehaviour
+{
+    public GameObject ennemie;
+    [SerializeField]
+    private Image gaugeHP;
+    private float percentHP;
+
+    public FloatReference enemieMaxHP;
+    public FloatVariable currentEnnemieHP;
+
+    void Start()
+    {
+        percentHP = 1F;
+    }
+
+    void Update()
+    {
+        //percentHP = currentHP / playerMaxHP;
+
+        gaugeHP.fillAmount = percentHP;
+    }
+}
