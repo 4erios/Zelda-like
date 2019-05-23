a2bf80f7ffbc8f71e7e4102e6f0e17c4e18cfe7e
Nagashka <e.potdevin@rubika-edu.com>
2019-05-24 01:39:27 +0200

    Ui + Début spawners

---
+++ Zelda-Like/Assets/Scripts/LD/AreaManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaManager : MonoBehaviour
+{
+    #region Demarage
+    public static AreaManager manager;
+
+    private void Awake()
+    {
+        if (manager == null)
+        {
+            manager = this;
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+    #endregion
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
