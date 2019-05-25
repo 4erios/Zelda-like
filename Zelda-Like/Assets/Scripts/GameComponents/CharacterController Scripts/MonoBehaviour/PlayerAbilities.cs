5974e7a4c9984130098938a5e743f347f50bcd5a
Aeri0s <senac.seconde@gmail.com>
2019-05-25 23:32:20 +0200

    Fix layer abilities

--- Zelda-Like/Assets/Scripts/GameComponents/CharacterController Scripts/MonoBehaviour/PlayerAbilities.cs
+++ Zelda-Like/Assets/Scripts/GameComponents/CharacterController Scripts/MonoBehaviour/PlayerAbilities.cs
@@ -35,6 +35,7 @@ public class PlayerAbilities : MonoBehaviour
     public FloatReference AOEInfuseKnockBackDistance;
     public FloatReference AOEInfuseDamages;
     public Transform AOEEmissionPoint;
+    public LayerMask LayerToAttack;
 
     public FloatVariable PlayerDamagesTaken;
     public FloatReference ShieldDamageTaken;
@@ -117,7 +118,7 @@ public class PlayerAbilities : MonoBehaviour
     {
         if (EnergyGauge >= AOEInfuseCost)
         {
-            Collider2D[] enemiesHurt = Physics2D.OverlapCircleAll(AOEEmissionPoint.position, AOEInfuseRange);
+            Collider2D[] enemiesHurt = Physics2D.OverlapCircleAll(AOEEmissionPoint.position, AOEInfuseRange, LayerToAttack);
             foreach (Collider2D enemyCollision in enemiesHurt)
             {
                 enemyCollision.GetComponent<LivingClass>().TakeDamages(AOEInfuseDamages);
@@ -146,7 +147,7 @@ public class PlayerAbilities : MonoBehaviour
     {
         if(EnergyGauge >= ShootInfuseCost)
         { 
-            RaycastHit2D hit = Physics2D.Raycast(FirePoint.position, Vector2.right);
+            RaycastHit2D hit = Physics2D.Raycast(FirePoint.position, Vector2.right, LayerToAttack);
             if (hit.collider != null)
             {
                 FireDirection = new Vector2(hit.point.x - FirePoint.position.x, hit.point.y - FirePoint.position.y);
