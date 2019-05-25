797359f05e14dfaa9a4e19910b2cebce06511814
Nagashka <e.potdevin@rubika-edu.com>
2019-05-25 16:13:47 +0200

    Preparation Reset Mort

---
+++ Zelda-Like/Assets/Scripts/GameComponents/CharacterController Scripts/Dead_Dead.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dead_Dead : MonoBehaviour
+{
+    public bool reallyDead;
+
+    [Header("Reset")]
+    public FloatReference maxHP;
+    public FloatVariable currentHP;
+    public FloatReference maxEnergy;
+    public FloatVariable currentEnergy;
+    public FloatVariable currentSouls;
+    public GameObject arenasManager;
+    public int gateScene;
+
+    void Update()
+    {
+        if (reallyDead)
+        {
+            ReallyDead();
+        }
+    }
+
+    public void ReallyDead()
+    {
+        currentHP.SetFloatValue(maxHP);
+        currentEnergy.SetFloatValue(maxEnergy);
+        currentSouls.SetFloatValue(0);
+        arenasManager.GetComponent<ArenasManager>().ResetArenas();
+    }
+}
