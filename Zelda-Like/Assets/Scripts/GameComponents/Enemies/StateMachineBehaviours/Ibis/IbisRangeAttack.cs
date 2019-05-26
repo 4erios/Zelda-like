a7cae3eb19c1140f492635bb125ee7dab898e996
Aeri0s <senac.seconde@gmail.com>
2019-05-27 00:05:29 +0200

    ibis: need some fixing but works

---
+++ Zelda-Like/Assets/Scripts/GameComponents/Enemies/StateMachineBehaviours/Ibis/IbisRangeAttack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IbisRangeAttack : StateMachineBehaviour
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        animator.GetComponent<IbisClass>().SetIbisVelocityToZero();
+        animator.GetComponent<IbisClass>().StopDelayBetweenAttacks();
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        animator.GetComponent<IbisClass>().DelayBetweenAttacks();
+        animator.GetComponent<IbisClass>().ShootAcidSpray();
+    }
+}
---
+++ Zelda-Like/Assets/Scripts/GameComponents/Enemies/StateMachineBehaviours/Ibis/IbisTakeDamages.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IbisTakeDamages : StateMachineBehaviour
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        animator.GetComponent<LivingClass>().SetBooltakeDamagesToFalse();
+        animator.GetComponent<IbisClass>().SetKnockBackDirection();
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        animator.GetComponent<IbisClass>().KnockBack();
+    }
+}
