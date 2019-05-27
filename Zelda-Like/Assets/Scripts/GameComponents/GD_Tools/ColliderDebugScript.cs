b3276b216bc26dc7e758e117efec25301371dea0
Nagashka <e.potdevin@rubika-edu.com>
2019-05-27 12:48:38 +0200

    Matin Prez

---
+++ Zelda-Like/Assets/Scripts/GameComponents/GD_Tools/ColliderDebugScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderDebugScript : MonoBehaviour
+{
+    private bool disableCollider = false;
+    public Collider2D collider1;
+    //public Collider2D collider2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PressDebugTouch();
+        DisableCollider();
+    }
+
+    public void DisableCollider()
+    {
+        if (disableCollider == false)
+        {
+            collider1.enabled = true;
+            //collider2.enabled = true;
+        }
+        else if (disableCollider == true)
+        {
+            collider1.enabled = false;
+            //collider2.enabled = false;
+        }
+    }
+
+    public void PressDebugTouch()
+    {
+        if (Input.GetKeyDown("P"))
+        {
+            if (disableCollider == false)
+            {
+                disableCollider = true;
+            }
+            else if (disableCollider == true)
+            {
+                disableCollider = false;
+            }
+        }
+
+    }
+
+}
